df9ab3bbc463d6fdf0da9d04e3755be81dec1992
Gyhth <gerald.merkley@gmail.com>
2018-07-09 16:11:03 -0400

    Initial Development

---
+++ ServerInfo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+
+namespace ServerInfo
+{
+    class Program
+    {
+
+        private static readonly HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+
+            String path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\ServerInfo.txt";
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            Writefile fileWriter = new Writefile();
+            fileWriter.Output = new FileStream(path, FileMode.CreateNew);
+            var request = (HttpWebRequest)WebRequest.Create("https://reddit.com");
+            HttpWebResponse response;
+            String responseString;
+            try
+            {
+                request.Method = "HEAD";
+                response = (HttpWebResponse)request.GetResponse();
+                responseString = response.Headers.ToString();
+                fileWriter.writeToFile("HEAD");
+                fileWriter.writeToFile("--------------------------------------");
+                fileWriter.writeToFile(responseString);
+                fileWriter.close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        
+    }
+}
---
+++ ServerInfo/Writefile.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ServerInfo
+{
+    public class Writefile
+    {
+        private FileStream output;
+
+        public FileStream Output { get => output; set => output = value; }
+
+        public void WriteToBinaryFile<T>(string filePath, T objectToWrite, bool append = false)
+        {
+            using (Stream stream = File.Open(filePath, append ? FileMode.Append : FileMode.Create))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                binaryFormatter.Serialize(stream, objectToWrite);
+            }
+        }
+
+        public T ReadFromBinaryFile<T>(string filePath)
+        {
+            using (Stream stream = File.Open(filePath, FileMode.Open))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                return (T)binaryFormatter.Deserialize(stream);
+            }
+        }
+
+        private void writetoFile(String text)
+        {
+            Byte[] info = new UTF8Encoding().GetBytes(text + Environment.NewLine);
+            Output.Write(info, 0, info.Length);
+        }
+
+        public void writeToFile(String text, Boolean append = false)
+        {
+                this.writetoFile(text);
+        }
+
+        public void close()
+        {
+            Output.Close();
+        }
+    }
+}
